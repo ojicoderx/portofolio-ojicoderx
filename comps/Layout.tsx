6c7b46d43c89dec5bb923ca418f449d9f5ede6d9
ojicoderx <oredigitalshop@gmail.com>
2024-12-05 11:02:58 +0700

    Update Layout.tsx

--- comps/Layout.tsx
+++ comps/Layout.tsx
@@ -25,6 +25,14 @@ const Layout = ({ children }: props) => {
         <div className="flex flex-col">
           <TopNav />
           <div className="scrollbar h-[calc(100vh-4.5rem)] overflow-auto pb-[72px] sm:h-screen sm:pb-0 sm:pt-[4.5rem]">
+            {/* Gambar di bagian atas */}
+            <div className="mt-16 flex justify-center">
+              <img 
+                src="https://pomf2.lain.la/f/bbpa0ud1.png" 
+                alt="Top Image"
+                className="max-w-full h-auto"
+              />
+            </div>
             <div className="relative">{children}</div>
           </div>
           {isMobile ? <BottomNav /> : <div></div>}
